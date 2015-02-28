e7719c6bb66b02ce1f8882e94b5fee91123825c9
Sandeep Bhardwaj <sandeep.bhardwaj2307@gmail.com>
2015-02-28 23:00:12 +0530

    Sandeep | Added code to get port by heroku

--- app/server.js
+++ app/server.js
@@ -11,7 +11,7 @@ require('./routes')(app);
 module.exports = app;
 
 var hostname = process.argv[2] || config.ip;
-var port = process.argv[3] || config.port;
+var port = process.env.PORT || config.port;
 
 app.listen(port);
 logger('Server running at http://' + hostname + ':' + port);
